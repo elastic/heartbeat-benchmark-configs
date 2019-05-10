ad298d739ba73b17538f3a7ea3177eb4cd300f42
Andrew Cholakian <andrew@andrewvc.com>
2019-05-09 20:39:18 -0500

    ruby gen
    V2
    
    Fix PID
    
    ERRs/Warns
    
    CSVify
    
    Improve output

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Run in this dir with `CLOUDID=MYCLOUDID CLOUDAUTH=MYPASSWORD DOMAIN=mydomain.com ./run.sh`
-find -name '*.yml' -print0 | xargs -0 chmod 0600; ; rm -rf data; mkdir -p hbhome/{data,logs}; heartbeat -c $PWD/configs/http-100/heartbeat.yml --path.home $PWD/hbhome --path.data $PWD/hbhome/data --path.logs $PWD/hbhome/logs
